dd9dbcc3a92298cb956a0a9e15ac4cf1e9875ffc
Jordan Fisher <jordan.efisher@gmail.com>
2024-10-27 16:19:01 -0700

    Fix var name

--- pinkytwirl-swift/Sources/swift/AppDelegate.swift
+++ pinkytwirl-swift/Sources/swift/AppDelegate.swift
@@ -157,7 +157,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         print("shouldSuppress: \(shouldSuppress!)")        
         let synthetic_keys = engine?.get_synthetic_events()
-        for i in 0..<result!.len() {
+        for i in 0..<synthetic_keys!.len() {
             print("Synthetic key to generate \(i): \(result![i])")
         }
 
